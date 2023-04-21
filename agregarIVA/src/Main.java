dd68d746b6551285ed3ac98d518dff8fd55a253c
Bruno <brunogonzalez7314@gmail.com>
2023-04-21 14:48:34 -0300

    hola

---
+++ agregarIVA/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(agregarIVA(000));
+
+    }
+    public static double agregarIVA(double precio){
+        return precio*1.21;
+    }
+}
