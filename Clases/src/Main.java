9aa5829173835e3ae2381fdddfd831f0cdd509d4
Bruno73p <122320665+Bruno73p@users.noreply.github.com>
2023-05-18 23:34:54 -0300

    Delete Clases directory

--- Clases/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Gato Nisman= new Gato(4,"Angora","largo","blanco y negro", true,10,true,true,7.5 );
-        System.out.println(Nisman);
-    }
-}
